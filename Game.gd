5862ffb5ce8bfaf0a5d9f090de6918123b0b8b7d
Brad S <birdgeekthree@gmail.com>
2024-01-18 15:22:18 -0700

    Init

---
+++ Game.gd
@@ -0,0 +1,41 @@
+extends Node
+
+@onready var currentMoney = 0
+@onready var currentMP = 1
+@onready var money = $UI/money
+@onready var mp = $UI/mp
+@onready var gameTimer = $Timer
+@onready var nextLevelCost = 10
+@onready var status = $UI/statusLabel
+@onready var nextLevelCostLabel = $UI/nlc
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	startGame()
+
+func startGame():
+	currentMoney = 0
+	currentMP = 1
+	gameTimer.start()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	money.text = str(currentMoney)
+	mp.text = str(currentMP)
+	nextLevelCostLabel.text = str(nextLevelCost)
+
+func _on_timer_timeout():
+	currentMoney += (currentMP * 1)
+
+
+func _on_buy_mp_pressed():
+	if currentMoney >= nextLevelCost:
+		currentMP = currentMP * 2
+		currentMoney -= nextLevelCost
+		nextLevelCost = nextLevelCost*(nextLevelCost-5)
+		status.text = "Purchase successful!"
+	else:
+		status.text = "You cannot afford the next level!"
+
+
+func _on_grind_pressed():
+	currentMoney += 1

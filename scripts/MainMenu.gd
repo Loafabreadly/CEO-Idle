4319b54598633e795e2fffb122c7d6d84a72f92b
Brad S <birdgeekthree@gmail.com>
2024-01-18 20:44:27 -0700

    Restructure + Main Menu
    #2 should be mostly resolved, will still take some work to finish the options and spice it all up

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_game_btn_pressed():
+	print("Loading Game!")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_btn_pressed():
+	print("I was told to quit out via main menu button!")
+	get_tree().quit()
